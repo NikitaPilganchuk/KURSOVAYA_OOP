815d0bfe8a1ede84df829d15fc255c4c51526d4e
Nikita Pilganchuk <Nikita Pilganchuk>
2023-10-05 22:14:42 +0300

    fix

--- src/vacancy_classes/vacancy_superjob.py
+++ src/vacancy_classes/vacancy_superjob.py
@@ -5,21 +5,22 @@ class VacancySJ(Vacancy):
 
     def __init__(self):
         self.params = {
-            'text': None,
-            "area": 1,  # (1 is Moscow)
-            "per_page": None,
-            'salary_from': None,
-            'only_with_salary': True,
+            'keyword': None,
+            'payment_from': None,
+            'no_agreement': 1
             # 'experience': None,
         }
 
     def print_vacancies(self):
         with open('../vacancies_super_job.json', 'r', encoding='utf-8') as f:
             vacancies_list = json.load(f)
+            vacancies_list = sorted(vacancies_list, key=lambda x: x['Зарплата'])
+            num = 1
             for vacancy in vacancies_list:
-                print(f'Вакансия No {vacancy["Вакансия No"]}')
+                print(f'Вакансия No {num}')
                 print(f"Должность: {vacancy['Должность']}")
-                print(f"Зарплата от {vacancy['Зарплата от']} до {vacancy['Зарплата до']}")
+                print(f"Зарплата от {vacancy['Зарплата']}")
                 print(f"Ссылка: {vacancy['Ссылка']}")
                 print(f"Обязанности: \n{vacancy['Обязанности']}")
                 print()
+                num += 1
