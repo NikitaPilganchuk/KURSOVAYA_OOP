7c7e4ba007e4056087cc2389750f3e3a34489fb7
Nikita Pilganchuk <Nikita Pilganchuk>
2023-10-05 22:14:31 +0300

    fix

--- src/json_save_classes/json_saver_hhru.py
+++ src/json_save_classes/json_saver_hhru.py
@@ -1,12 +1,11 @@
 from KURSOVAYA_OOP.src.json_save_classes.json_saver import JsonSaver
 import json
 
-class JsonSaveHHru(JsonSaver):
+class JsonSaverHHru(JsonSaver):
 
     def save(self, response):
         with open('../vacancies_HHru.json', 'w', encoding='utf-8') as f:
             vacancies_list = []
-            num = 1
             for i in response.json()['items']:
                 middle = 0
                 if i['salary']['from'] and i['salary']['to']:
@@ -16,15 +15,13 @@ class JsonSaveHHru(JsonSaver):
                 elif i['salary']['to'] and not i['salary']['from']:
                     middle = i['salary']['to']
                 vacancy = {
-                    'Вакансия No': num,
                     'Зарплата': middle,
                     'Должность': i['name'],
                     'Требования': i['snippet']['requirement'],
                     'Обязанности': i['snippet']['responsibility']
                 }
                 vacancies_list.append(vacancy)
-                num += 1
-            json.dump(vacancies_list, f)
+            json.dump(sorted(vacancies_list, key=lambda x: x['Зарплата']), f)
 
     def delete_vacancy(self, vacancy_number):
         with open('../vacancies_HHru.json', 'r', encoding='utf-8') as f:
